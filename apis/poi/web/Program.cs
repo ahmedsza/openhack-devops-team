8576ff69c31ab8d35ea34bd425f321e1a9c1fa8b
ahmedsza <37135205+ahmedsza@users.noreply.github.com>
2020-06-12 15:38:04 +0200

    Update Program.cs

--- apis/poi/web/Program.cs
+++ apis/poi/web/Program.cs
@@ -13,7 +13,20 @@ namespace poi
     {
        public static void Main(string[] args) => CreateHostBuilder(args).Build().Run();
 
-    
+            static void Demp()
+        {
+
+        }
+
+        static void Demp1()
+        {
+            throw new Exception();
+        }
+
+        static void Demo3()
+        {
+            var abc = "Server = tcp:openhackn82382w0sql.database.windows.net,1433; Initial Catalog = mydrivingDB; Persist Security Info = False; User ID = demousersa1; Password = demo@pass123; MultipleActiveResultSets = False; Encrypt = True; TrustServerCertificate = False; Connection Timeout = 30; ";
+  }
         public static IHostBuilder CreateHostBuilder(string[] args)
         {
             //used to read env variables for host/port
